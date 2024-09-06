b774c01c46f0311ea8b23b37178c75eb96ee95c1
CandraDev <candrasetiadev@gmail.com>
2024-09-06 07:54:03 +0700

    terakhir sampai asset, lanjut ke model

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1 class="text-primary">About Me</h1>
-<p>Halo, nama saya <?=$data['nama'];?>, umur saya <?=$data['umur'];?>, saya adalah seorang <?=$data['pekerjaan'];?>. </p>
+<div class="container">
+    <h1 class="text-primary mt-5">About Me</h1>
+    <img src="<?=BASEURL;?>/img/profile.jpg" alt="<?= $data['nama']; ?>" class="rounded-circle shadow">
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?>. </p>
+</div>
